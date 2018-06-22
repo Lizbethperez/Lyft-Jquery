dc0880e6c39fc515d78ddfdd1fe203b2aaaf48c7
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-06-22 10:50:29 -0500

    Se sube validacion del Codigo Random Generado

--- js/app.js
+++ js/app.js
@@ -27,25 +27,41 @@ $('.input-code').on('input', function () {
       $('#phone').attr('disabled','disabled');;
     }
  }
-
-  function getCode(){
-      var codeRandom = Math.floor((Math.random() * 1000) + 1);
+var codeRandom=0;
+var getCode =function (){
+      codeRandom = Math.floor((Math.random() * 1000) + 1);
       if(codeRandom>=100){
         var numberToString=codeRandom.toString();
       swal( numberToString, "This is your Code!", "success");
-      return;
+      //countCode(codeRandom);
+      return codeRandom;
       }
-
-   
   }
-  function countCode(){
+  
+  function countCode(e,codeRandom1){
     var initialCode=$('#code').val();
-    if(initialCode.length==2){
-      $('#btn-next-modal2').removeClass('disabled'); // se le quita la clase al boton para que este se habilite
-      console.log(phone);
-    }else if(phone.length>2){
-      $('#btn-next-modal2').attr('disabled','disabled');;
+    console.log(initialCode,codeRandom1);
+  if(initialCode.length==3){
+      if(initialCode==codeRandom1){
+        swal( "good job", "This is your Code!", "success")
+        .then(function(){
+          $('#btn-next-modal2').removeClass('disabled'); // se le quita la clase al boton para que este se habilite
+          location.href="index3.html"
+        });
+        
+      }
     }
+    /*if(initialCode.length==2){
+      $('#btn-next-modal2').removeClass('disabled'); // se le quita la clase al boton para que este se habilite
+      console.log(initialCode);
+      if(initialCode===codeRandom1){
+        location.href="index3.html"
+      }else {
+        swal( "good job", "This is your Code!", "success");
+      }*/
+    /*}else if(phone.length2){
+      $('#btn-next-modal2').attr('disabled','disabled');;
+    }*/
 
   }
   
@@ -58,7 +74,7 @@ $(document).ready(function(){
     $('select').formSelect();
     $('#phone').keydown(countNumber);
     $('#btn-next').click(getCode);
-    $('#code').keydown(countCode);
+    $('#code').keyup(function(e){countCode(e,codeRandom) });
     $('#icon_name').keydown(activeButtonName);
   });
       
